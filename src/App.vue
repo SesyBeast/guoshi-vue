1bdcaeeb5e47bc784b14216876277653c3c28f7c
Beast <2554778850@qq.com>
2019-10-09 17:03:03 +0800

    amd64^dev@0.0.1

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   <Navigation></Navigation>
+  </div>
+</template>
+
+<script>
+
+import Navigation from './components/Navigation.vue'
+export default {
+  name: 'app',
+  components: {
+    Navigation,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        登陆页面
+    </div>
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style lang=scss scoped>
+    .aaa{
+        background:change-color($color: 1, $red: 1, $green: 2, $blue: 3, $hue: 0, $saturation: 0%, $lightness: 0%, $alpha: 1.0)
+        
+    }
+</style>>
